c4b6a7c48c43529af84af1ad4e6a2d926a4ec9ef
imagepointe-jk <josh.klope@imagepointe.com>
2023-10-20 12:39:16 -0500

    add post

--- app.js
+++ app.js
@@ -7,5 +7,10 @@ app.get("/", (req, res) => {
     res.status(200).send();
 })
 
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.status(200).send();
+})
+
 const port = process.env.PORT;
 app.listen(port || 3000, () => console.log(`Listening on port ${port}`));
